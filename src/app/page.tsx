bf66306defad24b18c955ac698ac5b5ee8a2f8fe
faseeh ali <34987986+FaseehFury@users.noreply.github.com>
2025-04-29 05:01:22 +0000

    main layout

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+   <Button variant="destructive" size="sm" className="text-grey-400">
+    click me
+   </Button>
+  );
+}
