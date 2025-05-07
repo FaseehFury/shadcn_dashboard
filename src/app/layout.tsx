7d5581d643ff5f1ff30d187fbc053bd00aa69ff3
faseeh ali <34987986+FaseehFury@users.noreply.github.com>
2025-05-07 06:43:15 +0000

    grid layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,7 @@ export  default async function RootLayout({
         <main className="w-full">
 
         <AppNavBar />
-        <div className=" ">
+        <div className="">
 
         {children}
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,14 @@ export default function Home() {
 
 
   return (
-   <>
-   <h1>dashboard</h1>
-   </>
+    <div className="grid grid-cols-1 sm:grid-col-2 2xl:grid-cols-4 gap-4">
+      <div className="bg-pink-300 lg:col-span-2">Box 1</div>
+      <div className="bg-blue-300 lg:col-span-2">Box 2</div>
+      <div className="bg-green-300 lg:col-span-2">Box 3</div>
+      <div className="bg-yellow-300 lg:col-span-2 row-span-3">Box 4</div>
+      <div className="bg-purple-300 lg:col-span-2">Box 5</div>
+      <div className="bg-red-300 lg:col-span-2">Box 6</div>
+      
+    </div>
   );
 }
